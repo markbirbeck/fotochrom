53bb1b1e0502e250064ea01cc9d3c5e1f3e98f72
Mark Birbeck <mark.birbeck@gmail.com>
2016-01-25 15:58:17 +0000

    add pie chart using google charts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   cohort: require('./lib/cohort'),
+  pie: require('./lib/pie'),
   timeSeries: require('./lib/time-series'),
   plotTimeSeries: require('./lib/plot-time-series')
 };
